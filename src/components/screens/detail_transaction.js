a998331d7827e65264f87a54bba3e1ddf7f9bd27
wahyukhairina <wkhairina.afzan@gmail.com>
2020-04-24 01:21:18 +0700

    main Navigation

---
+++ src/components/screens/detail_transaction.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import { View } from 'react-native';
+
+class DetailTransaction extends Compoennt {
+
+    render () {
+        return (
+            <>
+            <View>
+                <Text>
+                Ini detail transcation
+                </Text>
+            </View>
+            </>     
+        )
+    }
+ }
+
+ export default DetailTransaction
---
+++ src/components/screens/transaction_list.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import { View, Text } from 'react-native';
+
+class TransactionList extends Component {
+
+    render () {
+        return (
+            <>
+            <View>
+                <Text>
+                Ini transcation List
+                </Text>
+            </View>
+            </>     
+        )
+    }
+ }
+
+ export default TransactionList
